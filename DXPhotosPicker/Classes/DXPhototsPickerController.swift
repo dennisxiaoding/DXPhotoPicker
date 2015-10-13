53f2bbeeb69ce8f2ca3476ac4f3ec6f98f756ab9
dennis <dingxiao19900110@163.com>
2015-10-13 21:59:28 +0800

    commit
    commit

---
+++ DXPhotosPicker/Classes/DXPhototsPickerController.swift
@@ -0,0 +1,23 @@
+//
+//  DXPhototsPickerController.swift
+//  DXPhotosPicker
+//
+//  Created by Ding Xiao on 15/10/13.
+//  Copyright © 2015年 Dennis. All rights reserved.
+//
+
+import UIKit
+
+class DXPhototsPickerController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
