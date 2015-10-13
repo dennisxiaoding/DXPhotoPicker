e32654b7f0ae71408b8aaf4ad8179720971f34e0
dennis <dingxiao19900110@163.com>
2015-10-13 21:49:11 +0800

    init
    commit

---
+++ DXPhotosPicker/Support/UIColor+DXHex.swift
@@ -0,0 +1,57 @@
+//
+//  UIColor+DXHex.swift
+//  DXPhotosPickerDemo
+//
+//  Created by Ding Xiao on 15/10/13.
+//  Copyright © 2015年 Dennis. All rights reserved.
+//
+
+
+import UIKit
+
+extension UIColor {
+    convenience init(hexColor: String) {
+        var red:   CGFloat = 0.0
+        var green: CGFloat = 0.0
+        var blue:  CGFloat = 0.0
+        var alpha: CGFloat = 1.0
+        
+        var hexColorString = hexColor;
+        
+        if !hexColorString.hasPrefix("#"){
+            hexColorString = "#"+hexColor;
+        }
+        
+        let index   = hexColorString.startIndex.advancedBy(1)
+        let hex     = hexColorString.substringFromIndex(index)
+        let scanner = NSScanner(string: hex)
+        var hexValue: CUnsignedLongLong = 0
+        if scanner.scanHexLongLong(&hexValue) {
+            switch (hex.characters.count) {
+            case 3:
+                red   = CGFloat((hexValue & 0xF00) >> 8)       / 15.0
+                green = CGFloat((hexValue & 0x0F0) >> 4)       / 15.0
+                blue  = CGFloat(hexValue & 0x00F)              / 15.0
+            case 4:
+                red   = CGFloat((hexValue & 0xF000) >> 12)     / 15.0
+                green = CGFloat((hexValue & 0x0F00) >> 8)      / 15.0
+                blue  = CGFloat((hexValue & 0x00F0) >> 4)      / 15.0
+                alpha = CGFloat(hexValue & 0x000F)             / 15.0
+            case 6:
+                red   = CGFloat((hexValue & 0xFF0000) >> 16)   / 255.0
+                green = CGFloat((hexValue & 0x00FF00) >> 8)    / 255.0
+                blue  = CGFloat(hexValue & 0x0000FF)           / 255.0
+            case 8:
+                red   = CGFloat((hexValue & 0xFF000000) >> 24) / 255.0
+                green = CGFloat((hexValue & 0x00FF0000) >> 16) / 255.0
+                blue  = CGFloat((hexValue & 0x0000FF00) >> 8)  / 255.0
+                alpha = CGFloat(hexValue & 0x000000FF)         / 255.0
+            default:
+                print("Invalid RGB string, number of characters after '#' should be either 3, 4, 6 or 8")
+            }
+        } else {
+            print("Scan hex error")
+        }
+        self.init(red:red, green:green, blue:blue, alpha:alpha)
+    }
+}
